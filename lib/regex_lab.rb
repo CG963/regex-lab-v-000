7b8acaf982553dbb57d3fbc9722f2fce4bd4082a
Christopher Garcia <christopher.garcia08@gmail.com>
2018-11-23 19:12:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+word.grep(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.
 end
 
 def words_five_letters_long(text)
+text = ["afoot", "Excellent", "incredible", "Absolute", "unreal", "Inconceivable", "octopus"]
 
+text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.
 end
 
 def valid_phone_number?(phone)
-
+phone.
 end
